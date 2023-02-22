cc58c7240a7e1fae1c9c8fad056614baf6215aae
王凯 <wkcoder@163.com>
2023-02-22 10:55:39 +0800

    feat: build success

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from "@rollup/plugin-commonjs";
+import terser from '@rollup/plugin-terser';
 
 const pkg = require('./package.json');
 const libName = pkg.name;
@@ -25,15 +26,20 @@ export default {
       // 注意如果是umd格式的bundle的话name属性是必须的，这时可以在script标签引入后window下会挂载该属性的变量来使用你的类库方法
       name: libName,
       file: `dist/index.umd.js`,
+    },
+    {
+      // 通用格式可以用于node和browser等多个场景
+      format: 'umd',
+      // 注意如果是umd格式的bundle的话name属性是必须的，这时可以在script标签引入后window下会挂载该属性的变量来使用你的类库方法
+      name: libName,
+      file: `dist/index.min.js`,
+      plugins: [terser()]
     }
   ],
   plugins: [
-    typescript({
-      useTsconfigDeclarationDir: true,
-      sourceMap: false,
-    }),
     commonjs(),
     resolve(),
+    typescript(),
     babel({
       exclude: ['node_modules/**']
     }),
