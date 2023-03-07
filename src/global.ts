b35dc0401cd15e667d2d90ca36f823c5d40c777b
王凯 <wkcoder@163.com>
2023-03-07 14:39:41 +0800

    feat: add global function

--- src/global.ts
+++ src/global.ts
@@ -2,3 +2,5 @@ import $eventBus from './event';
 function global_ue_get_message(type: string, msg: unknown): void {
   $eventBus.emit('ue_message', type, msg)
 }
+
+window.global_ue_get_message = global_ue_get_message;
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,5 @@
 declare interface Window {
   ue: any;
   ue4: any;
+  global_ue_get_message: (type: string, mag: unknown) => void;
 }
